c07f81610cba63305eeb5fe7bc019a84f621c8a4
Yruhere88 <39745702+Yruhere88@users.noreply.github.com>
2021-05-10 17:47:53 -0400

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -26,13 +26,10 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
 
-$to = "haydensi@buffalo.edu";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: thank-you.html');
+
 
 
 // Function to validate against any email injection attempts
