b649e94a6ec451fe9793fdcf69755bb50d545383
Yruhere88 <39745702+Yruhere88@users.noreply.github.com>
2021-05-10 17:58:16 -0400

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -21,11 +21,14 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'haydensi@buffalo.edu';//<== update the email address
+$email_from = 'tom@amazing-designs.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
 
+$to = "haydensi@buffalo.edu";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
